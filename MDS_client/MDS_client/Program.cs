e0798b0977c04c2a846dd7f3d56a0bcab45f6b17
Rickard Ahlstedt <rickard.re.ahlstedt@gmail.com>
2015-11-27 11:49:04 +0100

    Påbörjat arbete för att kunna pusha till webbsidan. Dock så verkar det som att min apache-installation inte laddar php-modulen, så ska flytta databasen till min VM.

--- MDS_client/MDS_client/Program.cs
+++ MDS_client/MDS_client/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,22 +11,70 @@ namespace MDS_client
     class Program
     {
 
-        private static string clientID, clientKey, serverURL;
+        private static string clientID, clientKey, serverURL, postURL;
+        private static int serverPort;
 
         private static string input;
-
+        private static char[] sectionSplitter = { '[', ']'};
         static void Main(string[] args) {
             readConfig();
             Console.WriteLine("Using values: \n\tClientID: " + clientID
                                 + ", \n\tClientKey: " + clientKey
-                                + ", \n\tServer-URL: " + serverURL);
+                                + ", \n\tServer-URL: " + serverURL + ":" + serverPort);
             while (true) {
                 input = readInput(input);
-                Console.WriteLine(input);
+                //Console.WriteLine(input);
+                pushMessage(input);
                 input = "";
             }
         }
 
+        /*
+        * <summary>
+        *   Push the message to the server.
+        *   As well as construct the message, and create a timestamp.
+        * </summary>
+        */
+        private static void pushMessage(string s) {
+            DateTime time = new DateTime();
+            string timeString = "";
+            int priority = 0;
+            string[] sections = s.Split(sectionSplitter);
+            if (sections.Length > 1) {
+                timeString = sections[1];
+                time = Convert.ToDateTime(timeString);
+                Console.WriteLine(time.ToString("yyyy-MM-dd HH:mm:ss"));
+                priority = int.Parse(sections[3]);
+                Console.WriteLine(priority);
+                s = sections[4];
+                s = s.Replace("\n", "<br />");
+                Console.WriteLine(s);
+            } else {
+                time = DateTime.Now;
+                priority = 0;
+                s = sections[0];
+                s = s.Replace("\n", "<br />");
+                Console.WriteLine(s);
+            }
+
+            if (time != null && !string.IsNullOrWhiteSpace(s)) {
+                string URI = serverURL + ":" + serverPort + postURL;
+                Console.WriteLine(URI);
+                string parameters = "clientID=" + clientID + "&clientKey=" + clientKey + "&message=" + s + "&prio=" + priority + "&time=" + time.ToString("yyyy-MM-dd HH:mm:ss");
+                using (WebClient wc = new WebClient()) {
+                    wc.Headers[HttpRequestHeader.ContentType] = "application/x-www-form-urlencoded";
+                    string HtmlResult = wc.UploadString(URI, parameters);
+                    Console.WriteLine(HtmlResult);
+                }
+            }
+
+        }
+
+        /*
+        * <summary>
+        *   Read from the console-input, and return the value.
+        * </summary>
+        */
         private static string readInput(string s) {
             return s = Console.ReadLine();
         }
@@ -39,6 +88,8 @@ namespace MDS_client
             clientID = ConfigurationManager.AppSettings["clientID"];
             clientKey = ConfigurationManager.AppSettings["clientKey"];
             serverURL = ConfigurationManager.AppSettings["serverURL"];
+            postURL = ConfigurationManager.AppSettings["postURL"];
+            serverPort = int.Parse(ConfigurationManager.AppSettings["serverPort"]);
         }
     }
 }
